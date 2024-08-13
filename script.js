bf084cf7ea28ecc8541f835620dac8f8715fe9f3
JuanTalyson <juantalyson05@gmail.com>
2024-08-13 09:39:01 -0300

    alteração-codigo

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 let dom = document
 
-let h1 
+let titulo = document.getElementById("titulo").value;
+let nome = document.getElementById("nome").value;
+
+console.log(titulo); 
+
+function submit(){
+    let nome = document.getElementById ("nome").value;
+    let cpf = document.getElementById ("idade").value;
+
+    alert("Erro na pagina!");
+    console.log(nome);
+    console.log(validaCPF(cpf));
+}
+
+function validaCPF (cpf) {            
+    if(cpf = ""){ 
+        alert("Campo CPF não pode ser vazio"); 
+        return false; 
+    }
+
+    return true; 
+}
