00f3a93e3411480fbeabe6b28d5d1c8c79a70112
JuanTalyson <juantalyson05@gmail.com>
2024-08-20 10:34:48 -0300

    alteração-coding

--- script.js
+++ script.js
@@ -29,15 +29,41 @@ function validaCPF (cpf) {
         return false; 
     }
 
-    if(/[\d.-]+$/.test(cpf)){
+    if(!/^[\d.-]+$/.test(cpf)){
         console.log("O CPF só tem números ou ponto ou hífen");
-        return true;
+        return false;
     }
 
-    if(cpf.length = 11 && cpf.length != 14){
+    if(cpf.length != 11 && cpf.length != 14){
         alert("Formarto inválido!");
         return false;
     }
+    
+    // Interar 9 primeiros dígitos, respeitando a seguinte regra: 
+    let soma = 0;
+
+    for(let i = 1; i <=9; i++) {
+        // console.log(cpf.charAt(i-1));
+        soma = soma + (cpf.charAt(i-1) * (10 - (i-1)));
+        // recuperar o caractere i-1 da string cpf
+    }
+    console.log(soma);
+    let resto = soma % 11;
+    if(resto < 2 ){
+        if(cpf.charAt(9) != 0){
+            alert("CPF inválido!");
+            return false;
+        // verificar se o primeiro digito verificador 
+        // (10º digito do cpf) é zero 
+        // 10º digito do cpf é recuperado com o cpf.charAt(9)
+        }
+        return true;
+    }
+
+    let digitoVerificador = 1 - resto;
+
+   
+    
 
     // Continuar validção 
     // Formatos CPFs válidos
